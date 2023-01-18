94fcff6911a3632583fc0ac3fa8b21ae2f5635bf
Matt Langan <matt@Matts-Mac-Studio.local>
2023-01-18 01:27:21 -0600

    Prepare Calendar to use new non-Socrata endpoint

--- lib/calendar/import.rb
+++ lib/calendar/import.rb
@@ -1,28 +1,23 @@
 require 'soda/client'
 require 'fileutils'
 require 'yaml'
+require 'HTTParty'
 
 module Calendar
   class Import
 
     class << self
       def import(config)
-        results = socrata_client.get("e43u-d7ev", "$limit": 5000)
+        results = HTTParty.get('https://accdcalendar.blob.core.windows.net/webevents/calendarevents.json?sp=racwd&st=2023-01-11T17:01:55Z&se=2024-01-12T01:01:55Z&spr=https&sv=2021-06-08&sr=b&sig=mBEYw1CkQoFCkrqpszTDczkgCkpRufHb%2FRInV%2F%2FtKZY%3D')
         path = File.join(config["source"], config["data_dir"], "socrata", "events", "events.yaml")
 
         FileUtils.mkdir_p(File.dirname(path))
 
         File.open(path, "w") do |file|
-          file.write(YAML.dump(results.body.map(&:to_hash)))
+          file.write(YAML.dump(JSON.parse(results).map(&:to_hash)))
         end
       end
 
-      def socrata_client
-        @socrata_client ||= SODA::Client.new({
-          domain: "data.austintexas.gov",
-          app_token: ENV["SOCRATA_APP_TOKEN"]
-        })
-      end
     end
 
   end
